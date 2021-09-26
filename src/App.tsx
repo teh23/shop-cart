3e4b123ec8305cf5f36829321ffc5c536aabaedf
Andrzej Franek <andrzejfranek@hotmail.com>
2021-09-27 01:15:26 +0200

    add header; add some styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,26 @@ import {
 } from "react-router-dom";
 import Category from "./components/Category";
 import { fetcher } from "./lib/fetcher";
+import Header from "./components/Header";
 
 
 function App() {
 
+
+    //const [card, dispatch] = useReducer(reducer, initialState)
     const { data, error } = useSWR<string[]>('https://fakestoreapi.com/products/categories', fetcher)
     if (!data) return <>loading</>
 
     return (
         <>
             <BrowserRouter>
-                {data.map((category, idx) => {
-                    return (
-                        <Link to={category} key={idx} >{category} </Link>
-                    )
-                })}
+                <Header>
+                    {data.map((category, idx) => {
+                        return (
+                            <Link className={` border-b sm:p-2`} to={category} key={idx} >{category} </Link>
+                        )
+                    })}
+                </Header>
                 <Route path='/:category' component={Category} />
             </BrowserRouter>
         </>
