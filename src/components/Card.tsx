0bb8d879d7a8bef7db1b2aca67e7f3a85c168323
Andrzej Franek <andrzejfranek@hotmail.com>
2021-09-27 10:10:01 +0200

    add card

---
+++ src/components/Card.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ICard, useCard } from '../App'
+import { IProduct } from './Product'
+
+const Card = () => {
+    const { card, dispatch } = useCard()
+
+
+    const remove = (product: IProduct) => {
+        dispatch({
+            type: "REMOVE", payload: {
+                product: product,
+                count: 1
+            }
+        })
+    }
+    const changeCount = (product: ICard, e: React.ChangeEvent<HTMLInputElement>) => {
+
+        dispatch({
+            type: "CHANGE", payload: {
+                product: product.product,
+                count: Object.is(NaN, parseInt(e.target.value)) ? 0 : parseInt(e.target.value)
+            }
+        })
+    }
+    return (
+        <div>
+            {card.map((product, idx) => {
+
+                return (
+                    <div key={idx} className={`flex space-x-2`}><input className={`w-10`} onChange={(e) => changeCount(product, e)} value={product.count} type="number"></input><p>x {product.product.title} </p><button onClick={() => remove(product.product)}>remove</button></div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Card
