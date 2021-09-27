fbdcff0bf2f16f8bdb37447e930cf1a51bc38317
Andrzej Franek <andrzejfranek@hotmail.com>
2021-09-27 09:26:02 +0200

    connect with card;visual

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useCard } from '../App'
+import Stars from './Stars'
 
 export interface IProduct {
     category: string,
@@ -23,17 +25,39 @@ const Product = ({
     rating,
     title,
 }: IProduct) => {
-    console.log(title)
+    const { card, dispatch } = useCard()
+    const product = {
+        id: id,
+        category: category,
+        description: description,
+        image: image,
+        price: price,
+        rating: rating,
+        title: title
+    }
+    const addToCard = () => {
+        dispatch({
+            type: "ADD", payload: {
+                product: product,
+                count: 1
+            }
+        })
+    }
+
     return (
-        <div className={`rounded-xl shadow-lg bg-gray-400 p-5 sm:w-1/6 space-y-4 flex flex-col`}>
-            {category}
-            <img className={``} src={image} alt="title" />
-            <p className="font-bold">{title}</p>
-            <p>{description}</p>
-            <p>{price}</p>
-            <div className={`flex justify-between items-center`}>
-                <p>{rating.count}/{rating.rate}</p>
-                <button className={`bg-gray-100 rounded p-2 shadow font-bold text-sm `}>ADD</button>
+        <div className={`rounded-xl space-y-2 border-b p-2 sm:w-1/2 xl:w-1/3 h-40 flex justify-center`}>
+
+            <img className={`w-32`} src={image} alt="title" />
+            <div className={`flex flex-col p-2`}>
+                {/*category*/}
+                <p className="font-bold text-sm">{title}</p>
+                {/* <p>{description}</p> */}
+                <p className={`text-2xl font-bold`}>{price} zł</p>
+                <div className={`flex justify-between items-end`}>
+                    <div className={''}>{<Stars rating={rating.rate} />}<div className={`text-sm opacity-70`}>{rating.count} buyers</div></div>
+                    <button onClick={addToCard} className={` border-b p-2 shadow font-bold text-sm text-gray-700`}>ADD TO CARD</button>
+                </div>
+
             </div>
 
         </div>
